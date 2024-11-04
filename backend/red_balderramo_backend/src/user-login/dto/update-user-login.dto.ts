b7c84b9d89d9091d2b7fce4abce5d6b308d8c48a
Juani <latorre926@gmail.com>
2024-11-04 10:15:18 -0300

    subida archivos modificados (backend) // creación inicial user /auth

--- backend/red_balderramo_backend/src/user-login/dto/update-user-login.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserLoginDto } from './create-user-login.dto';
-import { IsNotEmpty } from 'class-validator';
-
-export class UpdateUserLoginDto extends PartialType(CreateUserLoginDto) {
-  @IsNotEmpty()
-  name: string;
-}
