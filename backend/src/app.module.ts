31b779b1119338b4044666d2ceeea1762dfd5a21
Juani <latorre926@gmail.com>
2024-11-04 12:49:54 -0300

    añadiendo user y auth, falta cambiar a prisma, y borrar datos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module'; // Adjust the import based on your project structure
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(), // Ensure this is present to load .env variables
+    TypeOrmModule.forRoot({
+      type: 'postgres', // or your database type
+      host: process.env.DB_HOST,
+      port: +5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule, // Import your user module or other modules here
+  ],
 })
 export class AppModule {}
+
