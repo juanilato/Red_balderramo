1636755797b4935416004e11f82396f2ad641456
Valentino <valentinoromeroj@gmail.com>
2024-11-04 11:58:35 -0300

    final live share

--- backend/src/auth/local.strategy.ts
+++ backend/src/auth/local.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from './auth.service';
+import { UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
