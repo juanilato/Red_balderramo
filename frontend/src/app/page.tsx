d93ab4fb4edca6ceaa21fb8eaa3223c4cccb158a
Joaquín Francisco Ruiz Orellano <joaquinruizorellano206@gmail.com>
2024-11-07 12:34:44 -0300

    Subir todo de nuevo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,37 +1,47 @@
-'use client'
+"use client";
 
-import React from "react";
-import {Form} from "../components/formulario"
+import React, { useState } from "react";
+import { Form } from "../components/formulario";
 
-function inicioSesion () {
+function InicioSesion() {
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (data) => {
+    try {
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: data.name,
+          password: data.password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error en el inicio de sesión");
+      }
+
+
+    } catch (error) {
+      setError(error.message);
+      console.error("Error de autenticación", error);
+    }
+  };
+
   return (
     <>
-      <Form title='Inicio Sesion' onSubmit={()=>{}} description="Formulario para iniciar Sesion">
+      <Form title="Inicio Sesion" onSubmit={handleLogin} description="Formulario para iniciar Sesion">
         <div className="my-[10px] flex flex-col gap-4">
-
-          <Form.Input
-            label='Usuario'
-            name='name'
-            placeholder="Usuario..."
-            type='text'
-          />
-
-          <Form.Input
-            label='Contraseña'
-            name='password'
-            placeholder="Contraseña..."
-            type='password'
-          />
-
+          <Form.Input label="Usuario" name="name" placeholder="Usuario..." type="text" />
+          <Form.Input label="Contraseña" name="password" placeholder="Contraseña..." type="password" />
         </div>
-
         <Form.SubmitButtom buttonText="Iniciar Sesion" />
-        
-        
+        {error && <p className="text-red-500">{error}</p>}
       </Form>
     </>
   );
-};
+}
 
-export default inicioSesion;
-
+export default InicioSesion;
