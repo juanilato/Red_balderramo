7cb40ad19efc21f2e6a88c2de2fd1f8cacdf37c5
Juani <latorre926@gmail.com>
2024-11-08 17:23:18 -0300

    eliminación ruta de creación de usuario en users, manejo completo de registro mediante auth

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,25 +1,15 @@
 import { Controller, Param, Post, Patch, Delete, Get, Body } from '@nestjs/common';
 import { UserService } from './users.service';
-import {CreateUserDto} from './dto/create-user.dto';
 import {UpdateUserDto} from './dto/update-user.dto';
-import { AuthService } from '../auth/auth.service';
 //controlador de usuario
 @Controller('users')
 export class UsersController {
   //crea objeto de servicio
   constructor(
     private readonly usersService: UserService,
-    private readonly authService: AuthService // Inyecta AuthService
+     
   ) {}
 
-
-   // Llama al servicio para crear el usuario le envía los datos necesarios
-  @Post('/create')
-  async create(@Body() createUserDto: CreateUserDto) {
-    const hashedPassword = await this.authService.hashPassword(createUserDto.password);
-    const userWithHashedPassword = { ...createUserDto, password: hashedPassword };
-    return this.usersService.createUser(userWithHashedPassword);
-  }
 
 
   // Llama al servicio para eliminar el usuario, le envía un id para realizar la búsqueda y eliminarlo.
@@ -47,5 +37,4 @@ export class UsersController {
   async showUsers(){
     return this.usersService.showUsers();
   }
-
 }
