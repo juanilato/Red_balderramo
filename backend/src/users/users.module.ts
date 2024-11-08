c914d812d43836b9497d5cc63d02859b85a60f9d
Juani <latorre926@gmail.com>
2024-11-08 17:23:18 -0300

    eliminación ruta de creación de usuario en users, manejo completo de registro mediante auth

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserService } from './users.service';
 import {UsersController} from './users.controller'
 import { PrismaModule } from '../../prisma/prisma.module';
-import { AuthService } from 'src/auth/auth.service';
+
 
 
 //módulo de usuario utiliza (módulo de prisma, y servicios de usuario)
 @Module({
   imports: [PrismaModule],
-  providers: [UserService,AuthService],
+  providers: [UserService],
   controllers: [UsersController],
   exports: [UserService],
 })
